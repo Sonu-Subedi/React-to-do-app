537f19a2ea872c9a8f641bc9dafafaa0e7e13ef9
Sonu Subedi <103894080+Sonu-Subedi@users.noreply.github.com>
2023-04-06 11:44:23 +0545

    Update AuthCheck.js

--- src/middleware/AuthCheck.js
+++ src/middleware/AuthCheck.js
@@ -3,15 +3,10 @@ import { useHistory } from "react-router-dom";
 const AuthCheck = () => {
   const history = useHistory();
 
-  //   if (localStorage.getItem("loggedIn")) {
-  //     // Do nothing...
-  //   } else {
-  //     history.replace("/login");
-  //   }
 
   !localStorage.getItem("loggedIn") && history.replace("/login");
 
-  //   localStorage.getItem("loggedIn") ? (() => {})() : history.replace("/login");
+
 };
 
 export default AuthCheck;
